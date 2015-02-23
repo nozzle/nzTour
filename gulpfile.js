b9d233a45a52a3cd57f9157ad304886e92c3cdab
Tanner Linsley <tannerlinsley@gmail.com>
2015-02-23 02:28:50 -0700

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglifyjs');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+
+
+var config;
+
+gulp.task('stylus', stylusTask);
+gulp.task('js', jsTask);
+gulp.task('watch', watchTask);
+gulp.task('build', ['stylus', 'js']);
+gulp.task('default', ['build', 'watch']);
+
+function stylusTask() {
+    gulp.src('./src/nz-tour.styl')
+        .pipe(stylus({
+            use: [nib()],
+            compress: false,
+        }))
+        .pipe(rename("nz-tour.css"))
+        .pipe(gulp.dest('./dist/'));
+
+    return gulp.src('./src/nz-tour.styl')
+        .pipe(stylus({
+            use: [nib()],
+            compress: true,
+        }))
+        .pipe(rename("nz-tour.min.css"))
+        .pipe(gulp.dest('./dist/'));
+}
+
+function jsTask() {
+    gulp.src('./src/nz-tour.js')
+        .pipe(uglify())
+        .pipe(rename('nz-tour.min.js'))
+        .pipe(gulp.dest('./dist/'));
+
+    return gulp.src('./src/nz-tour.js')
+        .pipe(gulp.dest('./dist/'));
+}
+
+function watchTask() {
+    gulp.watch('./src/**', ['build']);
+}
